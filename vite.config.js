86a0a8b73b240802ce9431b68e6c946e1248b659
Renat Sadykov <53468111+rntsdkv@users.noreply.github.com>
2026-06-13 13:08:33 +0300

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Если будете публиковать на GitHub Pages по адресу
-// https://<user>.github.io/<repo>/ — впишите сюда base: "/<repo>/"
 export default defineConfig({
-  base: "./",
+  base: "/FM_radio_IETR/",
   plugins: [react()],
 });
